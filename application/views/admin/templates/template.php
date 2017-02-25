ccc9412a54d1af0665e64946e88c08a7276a5289
jmsenosa <jmsenosa@xolveit.com>
2017-02-25 23:13:43 +0800

    change label

--- application/views/admin/templates/template.php
+++ application/views/admin/templates/template.php
@@ -27,7 +27,7 @@
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $this->session->userdata('acc_name'); ?><b class="caret"></b></a>
 						<ul class="dropdown-menu">
-							<li><a href="<?php echo site_url('admin/profile'); ?>">Profile</a></li>
+							<li><a href="<?php echo site_url('admin/profile'); ?>">Dashboard</a></li>
 							<li class="divider"></li>
 							<li><a href="<?php echo site_url('admin/index/logout'); ?>">Logout</a></li>
 						</ul>
